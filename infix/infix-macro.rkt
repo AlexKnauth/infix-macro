c33eaa9e60bb06f2128478870314eede35af8451
AlexKnauth <alexander@knauth.org>
2015-01-14 18:05:11 -0500

    separate literals from syntax-parameters
    so that another module can redefine an operation and use that within
    the macro while still having the operation named normally.

--- infix/infix-macro.rkt
+++ infix/infix-macro.rkt
@@ -1,30 +1,38 @@
 #lang racket/base
 
-(provide : + * - / ^)
+(provide : add mult neg inv ex)
 
 (require racket/stxparam
          (prefix-in rkt: racket/base)
          (for-syntax (except-in racket/base + * - /)
-                     syntax/parse))
+                     syntax/parse
+                     syntax/parse/define
+                     (for-syntax racket/base)))
 (module+ test
   (require rackunit))
 
-(define-syntax-parameter + (make-rename-transformer #'rkt:+))
-(define-syntax-parameter * (make-rename-transformer #'rkt:*))
-(define-syntax-parameter - (make-rename-transformer #'rkt:-))
-(define-syntax-parameter / (make-rename-transformer #'rkt:/))
-(define-syntax-parameter ^ (make-rename-transformer #'rkt:expt))
+(define-syntax-parameter add (make-rename-transformer #'rkt:+))
+(define-syntax-parameter mult (make-rename-transformer #'rkt:*))
+(define-syntax-parameter neg (make-rename-transformer #'rkt:-))
+(define-syntax-parameter inv (make-rename-transformer #'rkt:/))
+(define-syntax-parameter ex (make-rename-transformer #'rkt:expt))
 
 (begin-for-syntax
+  (define (orig stx)
+    (syntax-property stx 'original-for-check-syntax #t))
   (define disappeared-use 'disappeared-use)
   (define (disap stx . ids)
     (define stx.ids (or (syntax-property stx disappeared-use) '()))
     (syntax-property stx disappeared-use (append stx.ids (map syntax-local-introduce ids))))
-  (define-syntax-class + [pattern (~and (~literal +) +)])
-  (define-syntax-class * [pattern (~and (~literal *) *)])
-  (define-syntax-class - [pattern (~and (~literal -) -)])
-  (define-syntax-class / [pattern (~and (~literal /) /)])
-  (define-syntax-class ^ [pattern (~and (~literal ^) ^)])
+  (define-simple-macro (define-op-syntax-class id:id stxparam:id)
+    (define-syntax-class id
+      #:attributes (id)
+      [pattern (~and stx (~datum id)) #:with id (orig (syntax/loc #'stx stxparam))]))
+  (define-op-syntax-class + add)
+  (define-op-syntax-class * mult)
+  (define-op-syntax-class - neg)
+  (define-op-syntax-class / inv)
+  (define-op-syntax-class ^ ex)
   (define-syntax-class op
     [pattern (~or :+ :* :- :/ :^)])
   (define-syntax-class mexpr
@@ -33,27 +41,27 @@
     [pattern (~seq (~or a:product a:+-product))
              #:with norm #'a.norm]
     [pattern (~seq (~or a:product a:+-product) b:+-product ...)
-             #:with norm #'(+ a.norm b.norm ...)])
+             #:with norm #'(add a.norm b.norm ...)])
   (define-splicing-syntax-class product
     [pattern (~seq (~or a:mexpt a:*/expt))
              #:with norm #'a.norm]
     [pattern (~seq (~or a:mexpt a:*/expt) b:*/expt ...)
-             #:with norm #'(* a.norm b.norm ...)])
+             #:with norm #'(mult a.norm b.norm ...)])
   (define-splicing-syntax-class mexpt
     [pattern (~seq a:mexpr)
              #:with norm #'a.norm]
     [pattern (~seq a:mexpr ^:^ b:mexpr)
-             #:with norm #'(^ a.norm b.norm)])
+             #:with norm #'(^.^ a.norm b.norm)])
   (define-splicing-syntax-class +-product
     [pattern (~seq +:+ a:product)
-             #:with norm (disap #'a.norm #'+)]
+             #:with norm (disap #'a.norm #'+.+)]
     [pattern (~seq -:- a:product)
-             #:with norm #'(- a.norm)])
+             #:with norm #'(-.- a.norm)])
   (define-splicing-syntax-class */expt
     [pattern (~seq *:* a:mexpt)
-             #:with norm (disap #'a.norm #'*)]
+             #:with norm (disap #'a.norm #'*.*)]
     [pattern (~seq /:/ a:mexpt)
-             #:with norm #'(/ a.norm)])
+             #:with norm #'(/./ a.norm)])
   )
 (define-syntax :
   (syntax-parser

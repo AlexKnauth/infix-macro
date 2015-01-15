29300113b082b75d85d3615d7b2650ca948b8028
AlexKnauth <alexander@knauth.org>
2015-01-14 22:56:20 -0500

    refactoring with more macros

--- infix/general-infix-ct.rkt
+++ infix/general-infix-ct.rkt
@@ -12,6 +12,10 @@
          infix-parser->transformer
          defstxcls/infix-parser
          defstxcls/op
+         op/stxcls
+         op/stxparser
+         stxcls->infix-parser
+         stxcls-infix-parser/rec
          )
 
 (require syntax/parse
@@ -59,6 +63,23 @@
            [pattern stx:id #:when (symbol=? (syntax-e #'stx) sym)
                     #:with attr-id (orig (syntax/loc #'stx id-stx))])))
 
+(define-simple-macro (op/stxcls (stxcls-id:id #:sub-pat sub-pat) body ...+)
+  (begin (define (op p)
+           (defstxcls/infix-parser stxcls-id p #:sub-pat sub-pat)
+           body ...)
+         op))
+
+(define-simple-macro (op/stxparser (stxcls-id:id #:sub-pat sub-pat) opt-or-clause ...+)
+  (op/stxcls (stxcls-id #:sub-pat sub-pat) (syntax-parser opt-or-clause ...)))
+
+(define-simple-macro (stxcls->infix-parser stxcls)
+  (syntax-parser [((~var a stxcls)) #'a.norm]))
+
+(define-simple-macro (stxcls-infix-parser/rec stxcls-id:id stuff ...)
+  (begin (define-splicing-syntax-class stxcls-id stuff ...)
+         (stxcls->infix-parser stxcls-id)))
+           
+
 (define (orig stx)
   (syntax-property stx 'original-for-check-syntax #t))
 (define disappeared-use 'disappeared-use)
@@ -68,19 +89,12 @@
                          (map orig (append stx.ids (map syntax-local-introduce ids))))))
 
 ;; binary-op : [#:sym Sym #:id Id -> Infix-Op]
-(define (binary-op #:sym sym #:id id-stx_)
-  (defstxcls/op op-id #:sym sym #:attr op #:id id-stx_)
-  ;; op : Infix-Op
-  (define (op p)
-    (defstxcls/infix-parser pexpr p #:sub-pat (~not :op-id))
-    ;; parse : Infix-Parser
-    (define (parse stxlst)
-      (syntax-parse stxlst
-        [(a:pexpr) #'a.norm]
-        [(a:pexpr op:op-id b:pexpr)
-         #'(op.op a.norm b.norm)]))
-    parse)
-  op)
+(define (binary-op #:sym sym #:id id-stx)
+  (defstxcls/op op-id #:sym sym #:attr op #:id id-stx)
+  (op/stxparser (pexpr #:sub-pat (~not :op-id))
+    [(a:pexpr) #'a.norm]
+    [(a:pexpr op:op-id b:pexpr)
+     #'(op.op a.norm b.norm)]))
     
 
 ;; add-op/sub-like-op : [#:add-sym Sym #:neg-sym Sym #:add-id Id #:neg-id Id -> Infix-Op]
@@ -88,21 +102,15 @@
   (define/syntax-parse add add-id)
   (defstxcls/op + #:sym add-sym #:attr + #:id add-id)
   (defstxcls/op - #:sym neg-sym #:attr - #:id neg-id)
-  ;; op : Infix-Op
-  (define (op p)
-    (defstxcls/infix-parser pexpr p #:sub-pat (~not (~or :+ :-)))
+  (op/stxcls (pexpr #:sub-pat (~not (~or :+ :-)))
     (define-splicing-syntax-class +/-pexpr
       #:attributes (norm)
       [pattern (~seq +:+ a:pexpr) #:with norm (disap #'a.norm #'+.+)]
       [pattern (~seq -:- a:pexpr) #:with norm #'(-.- a.norm)])
-    ;; parse : Infix-Parser
-    (define (parse stxlst)
-      (syntax-parse stxlst
-        [((~or a:pexpr a:+/-pexpr)) #'a.norm]
-        [((~or a:pexpr a:+/-pexpr) b:+/-pexpr ...)
-         #'(add a.norm b.norm ...)]))
-    parse)
-  op)
+    (syntax-parser
+      [((~or a:pexpr a:+/-pexpr)) #'a.norm]
+      [((~or a:pexpr a:+/-pexpr) b:+/-pexpr ...)
+       #'(add a.norm b.norm ...)])))
 
 ;; add-op : Infix-Op
 (define add-op (add/sub-like-op #:add-sym '+ #:neg-sym '- #:add-id #'add #:neg-id #'neg))
@@ -120,18 +128,14 @@
 ;; let-op : Infix-Op
 (define (let-op p)
   (defstxcls/infix-parser pexpr p #:sub-pat (~not (~or #:let (~datum =) #:with #:as)))
-  (define-splicing-syntax-class let-expr #:attributes (norm) #:datum-literals (=)
+  (stxcls-infix-parser/rec let-expr #:attributes (norm) #:datum-literals (=)
     [pattern (~seq a:pexpr) #:with norm #'a.norm]
     [pattern (~seq #:let id:id = a:pexpr (~or (~seq #:in b:let-expr)
                                               (~and (~seq (~or #:let #:with) _ ...)
                                                     (~seq b:let-expr))))
              #:with norm #'(let ([id a.norm]) b.norm)]
     [pattern (~seq #:with a:pexpr #:as id:id (~optional #:in) b:let-expr)
-             #:with norm #'(let ([id a.norm]) b.norm)])
-  (define (parse stx)
-    (syntax-parse stx
-      [(a:let-expr) #'a.norm]))
-  parse)
+             #:with norm #'(let ([id a.norm]) b.norm)]))
 
 ;; let+-*/^-parser : Infix-Parser
 (define let+-*/^-parser
